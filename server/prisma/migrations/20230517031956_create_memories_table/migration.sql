7061bd61c67f4ec47ca48930a34083903c67e890
Jose Augusto <jaleonimartins@hotmail.com>
2023-05-18 15:46:03 -0300

    Aula 3

--- server/prisma/migrations/20230517031956_create_memories_table/migration.sql
+++ server/prisma/migrations/20230517031956_create_memories_table/migration.sql
@@ -2,7 +2,7 @@
   Warnings:
 
   - Added the required column `avatarUrl` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `gihubId` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `githubId` to the `User` table without a default value. This is not possible if the table is not empty.
   - Added the required column `login` to the `User` table without a default value. This is not possible if the table is not empty.
 
 */
@@ -21,7 +21,7 @@ CREATE TABLE "Memory" (
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_User" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "gihubId" INTEGER NOT NULL,
+    "githubId" INTEGER NOT NULL,
     "name" TEXT NOT NULL,
     "login" TEXT NOT NULL,
     "avatarUrl" TEXT NOT NULL
@@ -29,6 +29,6 @@ CREATE TABLE "new_User" (
 INSERT INTO "new_User" ("id", "name") SELECT "id", "name" FROM "User";
 DROP TABLE "User";
 ALTER TABLE "new_User" RENAME TO "User";
-CREATE UNIQUE INDEX "User_gihubId_key" ON "User"("gihubId");
+CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
 PRAGMA foreign_key_check;
 PRAGMA foreign_keys=ON;
